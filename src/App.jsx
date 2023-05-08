fc806f00d3e5d2ea40bfa7ae9c0674a1bdb8d0db
Michael Selchow <mselchow@pm.me>
2023-05-07 20:44:37 -0500

    Add basic Clerk login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import {
+    ClerkProvider,
+    SignedIn,
+    SignedOut,
+    UserButton,
+    RedirectToSignIn,
+} from "@clerk/clerk-react";
+
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+    throw new Error("Missing Clerk publishable key.");
+}
+
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 function App() {
-    return <>Hello World!</>;
+    return (
+        <ClerkProvider publishableKey={clerkPubKey}>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
+        </ClerkProvider>
+    );
 }
 
 export default App;
