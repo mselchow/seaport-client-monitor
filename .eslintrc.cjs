e1772d38b8a88b720c4d719200119c8bc9ade178
Michael Selchow <mselchow@pm.me>
2023-05-07 20:14:15 -0500

    Initial project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+    env: { browser: true, es2020: true, node: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react-hooks/recommended",
+        "prettier",
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: { version: "detect" },
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".jsx"],
+            },
+        },
+    },
+    plugins: ["react-refresh", "react", "import", "jsx-a11y"],
+    rules: {
+        "react-refresh/only-export-components": "warn",
+        "react/prop-types": 0,
+        "react/react-in-jsx-scope": 0,
+    },
+};
