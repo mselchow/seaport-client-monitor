b527a8f45eeede8a5a35a3a23cf56489459fa635
Michael Selchow <mselchow@pm.me>
2023-05-08 21:55:17 -0500

    Add border to Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,14 +3,14 @@ import seaportLogo from "./assets/seaport-logo.png";
 
 const Header = () => {
     return (
-        <div className="flex place-content-between items-center p-4">
+        <div className="flex place-content-between items-center border-b p-4">
             <div className="flex">
                 <img
                     src={seaportLogo}
                     className="mr-3 h-6 sm:h-9"
                     alt="Seaport Logo"
                 />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                <span className="self-center whitespace-nowrap text-xl font-semibold">
                     Client Monitor
                 </span>
             </div>
