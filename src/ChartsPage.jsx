47d7aba086258935a6de2b7968241678458caa23
Michael Selchow <mselchow@pm.me>
2023-05-13 12:18:36 -0500

    Componentize Chart from ChartsPage

--- src/ChartsPage.jsx
+++ src/ChartsPage.jsx
@@ -1,17 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { Spinner } from "@material-tailwind/react";
-import {
-    BarChart,
-    Bar,
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    LabelList,
-    Cell,
-} from "recharts";
+import Chart from "./Chart";
 import fetchClockifyData from "./fetchClockifyData";
 
-const ChartsPage = ({ title }) => {
+const ChartsPage = () => {
     const results = useQuery(["data"], fetchClockifyData);
 
     if (results.isLoading) {
@@ -27,47 +19,12 @@ const ChartsPage = ({ title }) => {
         { name: "Specified Technologies, Inc.", value: 97 },
     ];
 
-    const width = 850;
-    const height = chartData.length * 35 + 50;
-
     return (
-        <BarChart
-            layout="vertical"
-            width={width}
-            height={height}
-            data={chartData}
-            barCategoryGap={4}
-        >
-            <CartesianGrid stroke="#f5f5f5" />
-            <XAxis
-                type="number"
-                domain={[0, 120]}
-                tickCount={7}
-                tickMargin={8}
-            />
-            <YAxis dataKey="name" type="category" width={250} tickMargin={8} />
-            <Bar dataKey="value" fill="rgba(255, 159, 64, 0.4)">
-                <LabelList
-                    dataKey="value"
-                    position="right"
-                    offset={5}
-                    formatter={(value) => {
-                        return value + "%";
-                    }}
-                />
-                {chartData.map((bar, index) => {
-                    let barColor;
-                    if (bar.value < 50) {
-                        barColor = "rgba(255, 159, 64, 1.0)";
-                    } else if (bar.value < 75) {
-                        barColor = "rgba(255, 205, 86, 1.0)";
-                    } else {
-                        barColor = "rgba(255, 99, 132, 1.0)";
-                    }
-                    return <Cell key={`cell-${index}`} fill={barColor} />;
-                })}
-            </Bar>
-        </BarChart>
+        <div>
+            <Chart title="Managed Services" data={chartData} />
+            <Chart title="Block Hours" data={chartData} />
+            <Chart title="Projects" data={chartData} />
+        </div>
     );
 };
 
