0f24135f5945400bbe99060c31062ac68d263dd7
Michael Selchow <mselchow@pm.me>
2023-05-13 12:19:31 -0500

    Add SettingsPage

--- src/SettingsPage.jsx
+++ src/SettingsPage.jsx
@@ -1,5 +1,14 @@
+import { Input, Button } from "@material-tailwind/react";
+
 const SettingsPage = () => {
-    return;
+    return (
+        <div className="flex place-items-center gap-4">
+            <div className="w-96">
+                <Input label="Clockify API Key" />
+            </div>
+            <Button>Save</Button>
+        </div>
+    );
 };
 
 export default SettingsPage;
