65f962ba04ad0a1b31c4b8c1ad61c9dd9dec6e51
Michael Selchow <mselchow@pm.me>
2023-05-21 09:57:21 -0500

    Add basic sort functionality

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,11 +1,40 @@
 import { Card, Typography } from "@material-tailwind/react";
+import { useState } from "react";
 import { ChevronUpDownIcon } from "@heroicons/react/24/solid";
 
 // TODO: adjust below code to accommodate any array length for data
 const Table = ({ title, data, headers }) => {
+    const [tableData, setTableData] = useState(data);
+    const [sortField, setSortField] = useState("");
+    const [order, setOrder] = useState("asc");
+
     if (!data.length) {
         return;
     }
+
+    const handleSortingChange = (accessor) => {
+        const sortOrder =
+            accessor === sortField && order === "asc" ? "desc" : "asc";
+        console.log(accessor);
+        setSortField(accessor);
+        setOrder(sortOrder);
+        handleSorting(accessor, sortOrder);
+    };
+
+    const handleSorting = (sortField, sortOrder) => {
+        if (sortField) {
+            const sorted = [...tableData].sort((a, b) => {
+                return (
+                    a[sortField]
+                        .toString()
+                        .localeCompare(b[sortField].toString(), "en", {
+                            numeric: true,
+                        }) * (sortOrder === "asc" ? 1 : -1)
+                );
+            });
+            setTableData(sorted);
+        }
+    };
 
     return (
         <div className="pb-10">
@@ -16,9 +45,12 @@ const Table = ({ title, data, headers }) => {
                 <table className="w-full min-w-max table-auto text-left">
                     <thead>
                         <tr>
-                            {headers.map((head, index) => (
+                            {headers.map(({ label, accessor }) => (
                                 <th
-                                    key={head}
+                                    key={accessor}
+                                    onClick={() =>
+                                        handleSortingChange(accessor)
+                                    }
                                     className="cursor-pointer border-b border-blue-gray-100 bg-blue-gray-50 p-4 hover:bg-blue-gray-100"
                                 >
                                     <Typography
@@ -26,7 +58,7 @@ const Table = ({ title, data, headers }) => {
                                         color="blue-gray"
                                         className="flex items-center justify-between font-normal leading-none opacity-70"
                                     >
-                                        {head}{" "}
+                                        {label}{" "}
                                         <ChevronUpDownIcon
                                             strokeWidth={2}
                                             className="h-4 w-4"
@@ -37,7 +69,7 @@ const Table = ({ title, data, headers }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map(({ name, uid, hoursRemaining }) => (
+                        {tableData.map(({ name, uid, hoursRemaining }) => (
                             <tr key={uid} className="even:bg-blue-gray-50/50">
                                 <td className="p-2 px-4">
                                     <Typography
