a928aef5234802b76d4fb1055ee23356ff48a29f
Michael Selchow <mselchow@pm.me>
2023-05-22 08:16:44 -0500

    Move scrollbar from inner div to page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
     return (
         <>
             <Header />
-            <main className="container mx-auto flex justify-center overflow-y-scroll px-5 py-5">
+            <main className="container mx-auto flex justify-center  px-5 py-5">
                 {children}
             </main>
         </>
