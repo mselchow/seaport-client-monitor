81ab916a37cb40626e31aade8e572890610eb051
Michael Selchow <mselchow@pm.me>
2023-05-22 22:01:55 -0500

    Correct footer placement to bottom of page without cutting content off

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,12 +3,13 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
-        <>
+        <div className="flex min-h-screen flex-col justify-start">
             <Header />
             <main className="container mx-auto flex justify-center px-5 py-5">
                 {children}
             </main>
+            <div className="flex-grow">{""}</div>
             <Footer />
-        </>
+        </div>
     );
 }
