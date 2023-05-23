482134f710f3211b03681fb3bbd328f89da20623
Michael Selchow <mselchow@pm.me>
2023-05-23 15:45:03 -0500

    Add build ID env var to Next config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,17 @@ const nextConfig = {
             },
         ];
     },
+    webpack(config, { buildId, webpack }) {
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                "process.env": {
+                    BUILD_ID: JSON.stringify(buildId),
+                },
+            })
+        );
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
