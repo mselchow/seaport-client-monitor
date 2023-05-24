947cf5cc6458bfb06e53b279de791ccf658aa033
Michael Selchow <mselchow@pm.me>
2023-05-24 16:56:13 -0500

    Update Layout to include RefreshPrompt

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,17 @@
 import { SignedIn } from "@clerk/nextjs";
 import Header from "./Header";
+import RefreshPrompt from "./RefreshPrompt";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col justify-start">
+        <div className="relative flex min-h-screen flex-col justify-start">
             <Header />
             <main className="container mx-auto flex justify-center px-5 py-5">
                 {children}
             </main>
             <div className="flex-grow">{""}</div>
+            <RefreshPrompt />
             <SignedIn>
                 <Footer />
             </SignedIn>
