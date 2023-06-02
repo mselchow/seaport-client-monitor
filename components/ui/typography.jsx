d162f38e850660c9284b22423914be66abbb4015
Michael Selchow <mselchow@pm.me>
2023-06-02 11:34:04 -0500

    Add new TypographySmall component and add classname merging

--- components/ui/typography.jsx
+++ components/ui/typography.jsx
@@ -1,16 +1,33 @@
-const TypographyH3 = ({ children, ...props }) => (
+import { cn } from "@/lib/utils";
+
+const TypographyH3 = ({ className, children, ...props }) => (
     <h3
-        className="scroll-m-20 text-2xl font-semibold tracking-tight"
+        className={cn(className, "scroll-m-20 text-2xl font-semibold")}
         {...props}
     >
         {children}
     </h3>
 );
 
-const TypographyH4 = ({ children, ...props }) => (
-    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight" {...props}>
+const TypographyH4 = ({ className, children, ...props }) => (
+    <h4
+        className={cn(
+            className,
+            "scroll-m-20 text-xl font-semibold tracking-tight"
+        )}
+        {...props}
+    >
         {children}
     </h4>
 );
 
-export { TypographyH3, TypographyH4 };
+const TypographySmall = ({ className, children, ...props }) => (
+    <small
+        className={cn(className, "text-sm font-normal leading-none")}
+        {...props}
+    >
+        {children}
+    </small>
+);
+
+export { TypographyH3, TypographyH4, TypographySmall };
