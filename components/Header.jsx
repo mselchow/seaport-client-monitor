ce5fb7e67b7ebd186f9766c16f265e9544824ebc
Michael Selchow <mselchow@pm.me>
2023-06-03 17:03:50 -0500

    Replace responsive hamburger with Tailwind

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import {
     UserButton,
@@ -9,10 +9,11 @@ import {
     SignInButton,
     SignUpButton,
 } from "@clerk/clerk-react";
-import { Navbar, Collapse, IconButton } from "@material-tailwind/react";
+import { Menu, X } from "lucide-react";
 import { TypographyH4 } from "@/components/ui/typography";
 import { Button } from "@/components/ui/button";
 import NavLink from "./NavLink";
+import { cn } from "@/lib/utils";
 
 const navLinks = [
     { title: "Charts", path: "/charts" },
@@ -23,15 +24,8 @@ const navLinks = [
 const Header = () => {
     const [openNav, setOpenNav] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth >= 960 && setOpenNav(false)
-        );
-    }, []);
-
     const navList = (
-        <div className="mb-4 mt-6 flex flex-col gap-4 text-primary lg:mb-0 lg:mt-0 lg:flex-row">
+        <div className="mb-3 mt-6 flex flex-col gap-4 text-primary lg:mb-0 lg:mt-0 lg:flex-row">
             {navLinks.map(({ title, path }) => (
                 <NavLink key={title} title={title} path={path} />
             ))}
@@ -71,80 +65,54 @@ const Header = () => {
     );
 
     return (
-        <>
-            <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none px-4 py-3 pb-2 lg:px-8 lg:py-4">
-                <div className="grid grid-cols-2 items-center text-primary lg:grid-cols-3">
-                    <div className="flex">
-                        <Image
-                            src="/seaport-logo.png"
-                            alt="Seaport Logo"
-                            className="mr-3 h-9"
-                            height="36"
-                            width="36"
-                        />
-                        <TypographyH4 className="self-center">
-                            Client Monitor
-                        </TypographyH4>
-                    </div>
-
-                    <div className="hidden lg:block lg:justify-self-center">
-                        <SignedIn>{navList}</SignedIn>
-                    </div>
-
-                    <div className="hidden lg:flex lg:justify-self-end">
-                        <SignedIn>{clerkUserAvatar}</SignedIn>
-                        <SignedOut>{clerkLoginRegisterButtons}</SignedOut>
-                    </div>
-
-                    <IconButton
-                        variant="text"
-                        className=" h-6 w-6 justify-self-end text-inherit hover:bg-transparent focus:bg-transparent lg:hidden"
-                        ripple={false}
-                        onClick={() => setOpenNav(!openNav)}
-                    >
-                        {openNav ? (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                className="h-6 w-6"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        ) : (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        )}
-                    </IconButton>
+        <header className="sticky inset-0 z-10 h-max max-w-full rounded-none bg-white bg-opacity-80 px-4 py-3 pb-2 shadow-md backdrop-blur-2xl lg:px-8 lg:py-4">
+            <div className="grid grid-cols-2 items-center text-primary lg:grid-cols-3">
+                <div className="flex">
+                    <Image
+                        src="/seaport-logo.png"
+                        alt="Seaport Logo"
+                        className="mr-3 h-9"
+                        height="36"
+                        width="36"
+                    />
+                    <TypographyH4 className="self-center">
+                        Client Monitor
+                    </TypographyH4>
                 </div>
 
-                <Collapse open={openNav} className="px-2">
-                    <SignedIn>
-                        {navList}
-                        <hr className="my-2" />
-                        {clerkUserAvatar}
-                    </SignedIn>
+                <nav className="hidden lg:block lg:justify-self-center">
+                    <SignedIn>{navList}</SignedIn>
+                </nav>
+
+                <div className="hidden lg:flex lg:justify-self-end">
+                    <SignedIn>{clerkUserAvatar}</SignedIn>
                     <SignedOut>{clerkLoginRegisterButtons}</SignedOut>
-                </Collapse>
-            </Navbar>
-        </>
+                </div>
+
+                <button
+                    className="h-6 w-6 justify-self-end text-inherit hover:bg-transparent focus:bg-transparent lg:hidden"
+                    aria-label={openNav ? "Close the menu" : "Open the menu"}
+                    aria-expanded={openNav}
+                    onClick={() => setOpenNav(!openNav)}
+                >
+                    {openNav ? <X /> : <Menu />}
+                </button>
+            </div>
+
+            <div
+                className={cn(
+                    openNav ? "h-auto" : "invisible max-h-0",
+                    "flex w-full  flex-col overflow-hidden lg:hidden"
+                )}
+            >
+                <SignedIn>
+                    {navList}
+                    <hr className="my-1" />
+                    {clerkUserAvatar}
+                </SignedIn>
+                <SignedOut>{clerkLoginRegisterButtons}</SignedOut>
+            </div>
+        </header>
     );
 };
 
