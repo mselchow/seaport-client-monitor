517e440138eed444e7999198c63ef7e8d8d9affb
Michael Selchow <mselchow@pm.me>
2023-06-05 21:58:23 -0500

    Add util function to convert seconds to text format

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function secToTime(seconds: number) {
+    const minutes = Math.floor((seconds / 60) % 60);
+    const hours = Math.floor(seconds / (60 * 60));
+    let result = "";
+
+    if (hours >= 1) {
+        result += hours + "h";
+        if (minutes >= 1) {
+            result += " ";
+        }
+    }
+    if (minutes >= 1) {
+        result += minutes + "m";
+    }
+
+    return result;
+}
