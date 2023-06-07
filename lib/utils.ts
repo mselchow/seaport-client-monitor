afff23ab62600c35467acf7b64c4d0fd407eeba9
Michael Selchow <mselchow@pm.me>
2023-06-06 22:17:40 -0500

    Return 0h 0m for undefined

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function secToTime(seconds: number) {
+    if (seconds === undefined) {
+        return "0h 0m";
+    }
+
     const hours = Math.floor(seconds / (60 * 60));
     const minutes = Math.floor((seconds / 60) % 60);
 
