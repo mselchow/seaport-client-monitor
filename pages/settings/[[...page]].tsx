ae6bf54c943c644bf2a752a1fa5ea2c2883d755f
Michael Selchow <mselchow@pm.me>
2023-06-30 15:25:54 -0500

    Add new settings catch-all route with basic layout - side bar nav - content per page - active route identification

--- pages/settings/[[...page]].tsx
+++ pages/settings/[[...page]].tsx
@@ -1,5 +1,8 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
 
 import {
     Card,
@@ -9,9 +12,37 @@ import {
     CardContent,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
     const router = useRouter();
+
+    const settingsNav = [
+        {
+            title: "Clockify",
+            href: "/settings",
+            content: <p>Clockify</p>,
+        },
+        {
+            title: "Clients",
+            href: "/settings/clients",
+            content: <p>Clients</p>,
+        },
+        {
+            title: "Goals",
+            href: "/settings/goals",
+            content: <p>Goals</p>,
+        },
+        {
+            title: "Display",
+            href: "/settings/display",
+            content: <p>Display</p>,
+        },
+    ];
+
+    const page = settingsNav.find(({ href }) => href === router.asPath);
+    const pageContent =
+        page !== undefined ? page.content : "An error occurred.";
 
     return (
         <>
@@ -30,10 +61,26 @@ export default function Page() {
                     <Separator className="mb-6" />
 
                     <div className="flex flex-col space-y-4 lg:flex-row lg:space-x-6 lg:space-y-0">
-                        <aside className="lg:w-1/5">Aside</aside>
-                        <div className="flex-1 lg:max-w-2xl">
-                            {router.query.page}
-                        </div>
+                        <aside className="lg:w-1/5">
+                            <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
+                                {settingsNav.map((nav) => (
+                                    <Button
+                                        variant="ghost"
+                                        key={nav.href}
+                                        className={cn(
+                                            router.asPath === nav.href
+                                                ? "bg-muted hover:bg-muted"
+                                                : "hover:bg-transparent hover:underline",
+                                            "justify-start"
+                                        )}
+                                        asChild
+                                    >
+                                        <Link href={nav.href}>{nav.title}</Link>
+                                    </Button>
+                                ))}
+                            </nav>
+                        </aside>
+                        <div className="flex-1 lg:max-w-2xl">{pageContent}</div>
                     </div>
                 </CardContent>
             </Card>
