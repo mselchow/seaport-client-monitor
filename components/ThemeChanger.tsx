a58287be6e1d942ae8d96faeaef12268fe4887d4
Michael Selchow <mselchow@pm.me>
2023-07-09 10:02:06 -0500

    Add System option to ThemeChanger

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -3,21 +3,48 @@
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+} from "@/components/ui/dropdown-menu";
 import { Sun, Moon } from "lucide-react";
 
 export default function ThemeChanger() {
-    const { resolvedTheme, setTheme } = useTheme();
-    const alternateTheme = resolvedTheme == "light" ? "dark" : "light";
+    const { theme, setTheme } = useTheme();
 
     return (
-        <Button
-            variant="ghost"
-            size="icon"
-            className="px-0"
-            onClick={() => setTheme(alternateTheme)}
-        >
-            <Sun className="h-4 w-4 scale-100 dark:scale-0" />
-            <Moon className="absolute h-4 w-4 scale-0 dark:scale-100" />
-        </Button>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="px-0">
+                    <Sun className="h-4 w-4 scale-100 dark:scale-0" />
+                    <Moon className="absolute h-4 w-4 scale-0 dark:scale-100" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuRadioGroup value={theme}>
+                    <DropdownMenuRadioItem
+                        value="light"
+                        onClick={() => setTheme("light")}
+                    >
+                        Light
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem
+                        value="dark"
+                        onClick={() => setTheme("dark")}
+                    >
+                        Dark
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem
+                        value="system"
+                        onClick={() => setTheme("system")}
+                    >
+                        System
+                    </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 }
