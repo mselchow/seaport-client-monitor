04c992c72fe9b8779dca4613d6bb71d5603bc62a
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2024-08-25 19:37:42 -0500

    refactor(dashboard): refactor chart config to its own file specific to the dashboard
    change from complex function with multiple ambiguous parameters to page-specific config

--- components/dashboard/DashboardChart.tsx
+++ components/dashboard/DashboardChart.tsx
@@ -1,12 +1,8 @@
-import { Bar, BarChart, XAxis, LabelList } from "recharts";
+import { useTheme } from "next-themes";
+import ReactApexChart from "react-apexcharts";
 
+import getDashboardChartOptions from "@/components/dashboard/DashboardChartOptions";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import {
-    ChartConfig,
-    ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
-} from "@/components/ui/chart";
 import { Skeleton } from "@/components/ui/skeleton";
 
 interface ChartDataType {
@@ -21,24 +17,37 @@ interface DashboardChartProps {
     isLoading: boolean;
 }
 
-const chartConfig = {
-    hours: {
-        label: "Hours",
-        color: "hsl(var(--primary))",
-    },
-} satisfies ChartConfig;
-
 export default function DashboardChart({
     title,
     data,
     isLoading = false,
 }: DashboardChartProps) {
+    const { resolvedTheme } = useTheme();
+
+    if (!resolvedTheme) {
+        return null;
+    }
+
+    const chartOptions = getDashboardChartOptions(resolvedTheme);
+
+    const dataMap = data.map(({ day, hours }) => ({
+        x: day,
+        y: hours,
+    }));
+
+    const series = [
+        {
+            name: "Hours Logged",
+            data: dataMap,
+        },
+    ];
+
     return (
         <Card>
             <CardHeader>
                 <CardTitle className="text-xl">{title}</CardTitle>
             </CardHeader>
-            <CardContent className="h-5/6 px-2">
+            <CardContent className="h-5/6">
                 {isLoading ? (
                     <div className="flex items-end justify-around">
                         <Skeleton className="h-[125px] w-[11%]" />
@@ -50,34 +59,13 @@ export default function DashboardChart({
                         <Skeleton className="h-[125px] w-[11%]" />
                     </div>
                 ) : (
-                    <ChartContainer
-                        config={chartConfig}
-                        className="h-full min-h-[500px] w-full"
-                    >
-                        <BarChart accessibilityLayer data={data}>
-                            <XAxis
-                                dataKey="day"
-                                tickLine={false}
-                                tickMargin={10}
-                                axisLine={false}
-                                tickFormatter={(value) => value.slice(0, 3)}
-                            />
-                            <ChartTooltip content={<ChartTooltipContent />} />
-                            <Bar
-                                dataKey="hours"
-                                fill="var(--color-hours)"
-                                radius={4}
-                            >
-                                <LabelList
-                                    position="top"
-                                    dataKey="label"
-                                    offset={12}
-                                    className="fill-primary"
-                                    fontSize={14}
-                                />
-                            </Bar>
-                        </BarChart>
-                    </ChartContainer>
+                    <ReactApexChart
+                        options={chartOptions}
+                        series={series}
+                        type="bar"
+                        height="500px"
+                        width="100%"
+                    />
                 )}
             </CardContent>
         </Card>
